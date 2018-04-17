e37abf7bec78608f2ebbf6d1d93d58a8991a186a
Jessica Campbell <jessicacampbell@zipcoders-MacBook-Pro-42.local>
2018-04-16 20:08:45 -0400

    finish

--- src/main/java/io/zipcoder/Problem6.java
+++ src/main/java/io/zipcoder/Problem6.java
@@ -1,82 +1,103 @@
 package io.zipcoder;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
+import static java.lang.Integer.min;
 import static java.lang.Integer.parseInt;
 import static java.lang.Integer.rotateLeft;
 
 public class Problem6 {
 
-    private String inputTime;
 
-    public Problem6(){
+    String[] stringsOfNums = {"Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten",
+            "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen", "Twenty",
+            "Twenty One", "Twenty Two", "Twenty Three"};
+
+    public Problem6() {
     }
 
-//    public String stringToSplit(String inputTime){
-//        ArrayList<Integer> splittingTime = new ArrayList<Integer>();
-//        String[] split = inputTime.split(" ");
-//        if (String)
-//    }
-
-    public String convertMorningHours(int hour){
-        String timeInMilitary = "";
-        switch (hour){
-            case 1: timeInMilitary = "Zero One";
-            break;
-            case 2: timeInMilitary = "Zero Two";
-                break;
-            case 3: timeInMilitary = "Zero Three";
-                break;
-            case 4: timeInMilitary = "Zero Four";
-                break;
-            case 5: timeInMilitary = "Zero Five";
-                break;
-            case 6: timeInMilitary = "Zero Six";
-                break;
-            case 7: timeInMilitary = "Zero Seven";
-                break;
-            case 8: timeInMilitary = "Zero Eight";
-                break;
-            case 9: timeInMilitary = "Zero Nine";
-                break;
-            case 10: timeInMilitary = "Ten";
-                break;
-            case 11: timeInMilitary = "Eleven";
-                break;
-            case 12: timeInMilitary = "Twelve";
-                break;
+    public String militaryTimeConverter(String input){
+        String output = "";
+        String hourMinute = "";
+        if (input.contains("am")){
+            hourMinute = input.replace("am", "");
+        } else if (input.contains("pm")){
+            hourMinute = input.replace("pm", "");
         }
-        return timeInMilitary;
+        String [] hourMinArray = hourMinute.split(":");
+        int [] hourMinIntegers = new int[2];
+        hourMinIntegers[0] = Integer.parseInt(hourMinArray[0]);
+        hourMinIntegers[1] = Integer.parseInt(hourMinArray[1]);
+
+        if (input.contains("am")){
+            output += morningConversionHour(hourMinIntegers[0]);
+            output += minuteConversion(hourMinIntegers[1]);
+        } else if (input.contains("pm")){
+            output += nightConversionHour(hourMinIntegers[0]);
+            output += minuteConversion(hourMinIntegers[1]);
+        }
+        return output;
     }
 
-    public String convertNightHours(int hour){
-        String timeInMilitary = "";
-        switch (hour){
-            case 1: timeInMilitary = "Thirteen";
-                break;
-            case 2: timeInMilitary = "Fourteen";
-                break;
-            case 3: timeInMilitary = "Fifteen";
-                break;
-            case 4: timeInMilitary = "Sixteen";
-                break;
-            case 5: timeInMilitary = "Seventeen";
-                break;
-            case 6: timeInMilitary = "Eighteen";
-                break;
-            case 7: timeInMilitary = "Nineteen";
-                break;
-            case 8: timeInMilitary = "Twenty";
-                break;
-            case 9: timeInMilitary = "Twenty One";
-                break;
-            case 10: timeInMilitary = "Twenty Two";
-                break;
-            case 11: timeInMilitary = "Twenty Three";
-                break;
-            case 12: timeInMilitary = "Zero Zero";
-                break;
+    public Map<Integer, String> populateMap() {
+        Map<Integer, String> numAndStringValue = new HashMap<Integer, String>();
+        for (int i = 0; i < stringsOfNums.length; i++) {
+            numAndStringValue.put(i, stringsOfNums[i]);
         }
-        return timeInMilitary;
+        numAndStringValue.put(30, "Thirty");
+        numAndStringValue.put(40, "Forty");
+        numAndStringValue.put(50, "Fifty");
+        return numAndStringValue;
+    }
+
+    public String morningConversionHour(int hour) {
+        Map<Integer, String> map = populateMap();
+        String result = "";
+        if (hour < 10) {
+            result += "Zero ";
+            result += map.get(hour);
+        } else if (hour == 12) {
+            result += "Zero Zero";
+        } else {
+            result += map.get(hour);
+        }
+        result += " Hundred and ";
+        return result;
+    }
+
+    public String nightConversionHour(int hour) {
+        Map<Integer, String> map = populateMap();
+        String result = "";
+        if (hour == 12) {
+            result += map.get(hour);
+        } else {
+            result += map.get(hour + 12);
+        }
+        result += " Hundred and ";
+        return result;
+    }
+
+    public String minuteConversion(int minute) {
+        Map<Integer, String> map = populateMap();
+        String result = "";
+        if (minute < 24) {
+            if (minute < 10) {
+                result += "Zero ";
+            }
+            result += map.get(minute);
+            result += " Hours";
+        } else {
+            int temp = minute / 10;
+            temp *= 10;
+            int modulus = minute % 10;
+            result += map.get(temp);
+            if (modulus != 0) {
+                result += map.get(modulus);
+            }
+            result += " Hours";
+        }
+        return result;
     }
 }
--- src/test/java/io/zipcoder/Problem6Test.java
+++ src/test/java/io/zipcoder/Problem6Test.java
@@ -1,4 +1,64 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class Problem6Test {
+
+    @Test
+    public void testConversion1(){
+        Problem6 problem6 = new Problem6();
+        String input = "1:30pm";
+        String expected = "Thirteen Hundred and Thirty Hours";
+        String actual = problem6.militaryTimeConverter(input);
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void testConversion2(){
+        Problem6 problem6 = new Problem6();
+        String input = "1:30am";
+        String expected = "Zero One Hundred and Thirty Hours";
+        String actual = problem6.militaryTimeConverter(input);
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void testConversion3(){
+        Problem6 problem6 = new Problem6();
+        String input = "2:22pm";
+        String expected = "Fourteen Hundred and Twenty Two Hours";
+        String actual = problem6.militaryTimeConverter(input);
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void testConversion4(){
+        Problem6 problem6 = new Problem6();
+        String input = "2:11am";
+        String expected = "Zero Two Hundred and Eleven Hours";
+        String actual = problem6.militaryTimeConverter(input);
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void testConversion5(){
+        Problem6 problem6 = new Problem6();
+        String input = "10:02am";
+        String expected = "Ten Hundred and Zero Two Hours";
+        String actual = problem6.militaryTimeConverter(input);
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void testConversion6(){
+        Problem6 problem6 = new Problem6();
+        String input = "12:00pm";
+        String expected = "Twelve Hundred and Zero Zero Hours";
+        String actual = problem6.militaryTimeConverter(input);
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void testConversion7(){
+        Problem6 problem6 = new Problem6();
+        String input = "12:00am";
+        String expected = "Zero Zero Hundred and Zero Zero Hours";
+        String actual = problem6.militaryTimeConverter(input);
+        Assert.assertEquals(expected, actual);
+    }
 }
